b583092b2aeeff17d5a5eb8fd09d654294e05dbb
enrico.cambiaso@gmail.com <enrico.cambiaso@gmail.com>
2015-05-14 08:17:36 +0000

    Test from heroku website

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn personalwebsite.wsgi -b 0.0.0.0:$PORT
+web: gunicorn personalwebsite.wsgi --log-file -
--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,4 @@
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "personalwebsite.settings")
-
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+from dj_static import Cling
 
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = Cling(get_wsgi_application())
