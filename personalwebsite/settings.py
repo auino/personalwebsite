f78d12c6e4c82a3de6cb31adeac7b47be2a76a1f
root <root@attacker>
2015-05-14 10:50:00 +0200

    retry 6

--- personalwebsite/settings.py
+++ personalwebsite/settings.py
@@ -62,8 +62,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-#STATIC_ROOT = 'static' # staticfiles
-STATIC_ROOT = os.path.join(os.getcwd(), "app/static")
+STATIC_ROOT = 'static' # staticfiles
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -75,7 +74,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(BASE_DIR, 'app/static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -85,6 +84,8 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'm6_95weo!x7xtoz22-g3tj1e0p=y*c3erkmt@yhzmcm&amp;4%vd41'
--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,7 @@
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
+from whitenoise.django import DjangoWhiteNoise
 
-<<<<<<< HEAD
 application = Cling(get_wsgi_application())
-=======
-application = Cling(get_wsgi_application())
->>>>>>> b583092b2aeeff17d5a5eb8fd09d654294e05dbb
+application = DjangoWhiteNoise(application)
+
