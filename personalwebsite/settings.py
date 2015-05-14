dcb1dc7e5f5689de45ad9cdad836a47700233429
root <root@attacker>
2015-05-14 11:25:54 +0200

    retry 10

--- personalwebsite/settings.py
+++ personalwebsite/settings.py
@@ -62,7 +62,8 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = 'static' # staticfiles
+#STATIC_ROOT = 'static' # staticfiles
+STATIC_ROOT = 'auinowebsite/static'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -75,6 +76,7 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     os.path.join(BASE_DIR, 'static'),
+    'auinowebsite/app/static',
 )
 
 # List of finder classes that know how to find static files in
--- personalwebsite/urls.py
+++ personalwebsite/urls.py
@@ -16,4 +16,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     #url(r'^admin/', include(admin.site.urls)),
+
+    (r'^static/', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
