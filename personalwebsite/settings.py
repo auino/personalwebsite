45262cb58c204f603ea12995f0d1f675acffff6d
root <root@attacker>
2015-05-14 10:46:18 +0200

    retry

--- personalwebsite/settings.py
+++ personalwebsite/settings.py
@@ -62,7 +62,8 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = 'static' # staticfiles
+#STATIC_ROOT = 'static' # staticfiles
+STATIC_ROOT = os.path.join(os.getcwd(), "app/static")
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -74,7 +75,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'app/static'),
 )
 
 # List of finder classes that know how to find static files in
