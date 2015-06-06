3c18219f13a7517cd86aec66651c89dc6a99478a
Enrico Cambiaso <auino@Alan.local>
2015-06-06 12:11:21 +0200

    added external url data source

--- app/views.py
+++ app/views.py
@@ -1,48 +1,11 @@
+import urllib, json
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-	configuration = {
-		'name': 'Enrico',
-		'surname': 'Cambiaso',
-		'role': 'Computer Scientist',
-		'social': [
-			{
-				'type': 'linkedin',
-				'name': 'LinkedIn',
-				'url': 'https://linkedin.com/in/enricocambiaso'
-			},
-			{
-				'type': 'twitter',
-				'name': 'Twitter',
-				'url': 'https://twitter.com/auino'
-			},
-			{
-				'type': 'googleplus',
-				'name': 'Google Plus',
-				'url': 'http://plus.google.com/+EnricoCambiaso'
-			},
-			{
-				'type': 'facebook',
-				'name': 'Facebook',
-				'url': 'https://www.facebook.com/enrico.cambiaso'
-			},
-		],
-		'links': {
-			'enabled': False,
-			'title': '',
-			'list': [
-				{
-					'url':'',
-					'name':''
-				},
-			]
-		},
-		'email': {
-			'address': 'enrico.cambiaso@gmail.com',
-			'tag': 'Auino website',
-			'question': 'Want to send me nerdish things?'
-		}
-	}
+	url = 'https://dl.dropboxusercontent.com/u/280149/auino.com/configuration.json'
+	response = urllib.urlopen(url);
+	configuration = json.loads(response.read())
+	
 	return render(request, 'index.html', configuration)
 
