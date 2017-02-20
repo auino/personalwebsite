f9ab4c496f333b78802e17dafe82965528e4acb1
Enrico Cambiaso <enricocambiaso@MacBook-Pro-di-Enrico.local>
2017-02-20 19:58:50 +0100

    Minor fix

--- app/views.py
+++ app/views.py
@@ -2,8 +2,8 @@ import urllib, json
 from django.shortcuts import render
 from django.conf import settings
 
-#BASEURL='http://www.auino.com' # DEBUG
-BASEURL=settings.SITE_URL
+BASEURL='http://www.auino.com' # DEBUG
+#BASEURL=settings.SITE_URL
 
 # Create your views here.
 def index(request):
@@ -11,4 +11,3 @@ def index(request):
 	response = urllib.urlopen(url);
 	configuration = json.loads(response.read())
 	return render(request, 'index.html', configuration)
-
