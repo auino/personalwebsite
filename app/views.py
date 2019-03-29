7770e65622c556364d52f575628a6b89d9467127
Enrico Cambiaso <enrico.cambiaso@gmail.com>
2019-03-29 15:01:51 +0100

    Trying to fix things

--- app/views.py
+++ app/views.py
@@ -9,4 +9,9 @@ def index(request):
 	url = BASEURL+'/static/configuration/configuration.json'
 	response = urllib.urlopen(url);
 	configuration = json.loads(response.read())
-	return render(request, 'index.html', configuration)
+	
+	from django.shortcuts import render_to_response
+	from django.template import RequestContext
+
+	return render_to_response('index.html', configuration, context_instance=RequestContext(request))
+	#return render(request, 'index.html', configuration)
