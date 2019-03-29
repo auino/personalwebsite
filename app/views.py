4eb9648f15308ac763ae51a8fbf197db3cf9fb6e
Enrico Cambiaso <enrico.cambiaso@gmail.com>
2019-03-29 14:45:06 +0100

    Trying to fix things

--- app/views.py
+++ app/views.py
@@ -1,10 +1,15 @@
 import urllib, json
 from django.shortcuts import render
 
+from django.template import loader, Context
+
 # main index view
 def index(request):
 	BASEURL=request.build_absolute_uri()
 	url = BASEURL+'/static/configuration/configuration.json'
 	response = urllib.urlopen(url);
 	configuration = json.loads(response.read())
+
+	t = loader.get_template('index.html')
+	c = Context({ 'object_list': None })
 	return render(request, 'index.html', configuration)
