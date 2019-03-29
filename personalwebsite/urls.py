4fc505013e889fe5a0d397a37e8e4afde446fb10
Enrico Cambiaso <enrico.cambiaso@gmail.com>
2019-03-29 10:16:14 +0100

    Minor bug fixes

--- personalwebsite/urls.py
+++ personalwebsite/urls.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.contrib import admin
 
 from django.conf.urls import static
-from django.conf.urls.defaults import *
+#from django.conf.urls.defaults import *
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
