51722ee5c9513131272bf164c9e8111e0313a2bc
Enrico Cambiaso <enrico.cambiaso@gmail.com>
2019-03-29 10:41:05 +0100

    Minor bug fixes

--- personalwebsite/settings.py
+++ personalwebsite/settings.py
@@ -116,14 +116,15 @@ WSGI_APPLICATION = 'personalwebsite.wsgi.application'
 SETTINGS_PATH = os.path.normpath(os.path.dirname(__file__))
 PROJECT_PATH = os.path.abspath(os.path.join(SETTINGS_PATH, os.pardir))
 
+print os.path.join(PROJECT_PATH, 'templates')
+
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     #'/tmp/pp/personalwebsite/app/templates'
     #os.path.join(SETTINGS_PATH, 'templates'),
-    #os.path.join(PROJECT_PATH, 'templates'),
-    PROJECT_PATH+'/templates/'
+    os.path.join(PROJECT_PATH, 'templates'),
 )
 
 INSTALLED_APPS = (
