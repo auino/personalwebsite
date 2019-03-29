9df328e4535c242fcbe5970f0cfba668144848c9
Enrico Cambiaso <enrico.cambiaso@gmail.com>
2019-03-29 15:02:51 +0100

    Trying to fix things

--- app/views.py
+++ app/views.py
@@ -1,8 +1,6 @@
 import urllib, json
 from django.shortcuts import render
 
-from django.template import loader, Context
-
 # main index view
 def index(request):
 	BASEURL=request.build_absolute_uri()
@@ -13,5 +11,5 @@ def index(request):
 	from django.shortcuts import render_to_response
 	from django.template import RequestContext
 
-	return render_to_response('index.html', configuration, context_instance=RequestContext(request))
+	return render_to_response('index.html', configuration, RequestContext(request))
 	#return render(request, 'index.html', configuration)
