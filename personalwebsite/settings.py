5af1be1842ac025295be8e008538fb4ac5c9d128
Enrico Cambiaso <enrico.cambiaso@gmail.com>
2019-03-29 14:52:34 +0100

    Trying to fix things

--- personalwebsite/settings.py
+++ personalwebsite/settings.py
@@ -118,7 +118,9 @@ WSGI_APPLICATION = 'personalwebsite.wsgi.application'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, '../app/templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
