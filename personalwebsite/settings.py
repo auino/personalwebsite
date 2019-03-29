35d4faf4d0482f67ff54912ae12f508cf5a425a8
Enrico Cambiaso <enrico.cambiaso@gmail.com>
2019-03-29 14:59:30 +0100

    Trying to fix things

--- personalwebsite/settings.py
+++ personalwebsite/settings.py
@@ -70,15 +70,12 @@ STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(BASE_DIR, '../app/static'),
-)
-
-STATIC_DIRS = (
     os.path.join(BASE_DIR, '../app/static'),
 )
 
