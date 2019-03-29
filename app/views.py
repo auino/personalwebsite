480a03be8f527191c8ab54889f52edfa2b0b36a1
Enrico Cambiaso <enrico.cambiaso@gmail.com>
2019-03-29 14:49:16 +0100

    Trying to fix things

--- app/views.py
+++ app/views.py
@@ -10,7 +10,18 @@ def index(request):
 	response = urllib.urlopen(url);
 	configuration = json.loads(response.read())
 
-	t = loader.get_template('templates/index.html')
-	c = Context({ 'object_list': None })
-	return t.render(c)
+	#t = loader.get_template('index.html')
+	#c = Context({ 'object_list': None })
+	#return t.render(c)
+
+	from django.conf import settings
+	from django.template.loaders.app_directories import app_template_dirs
+	import os
+	template_files = []
+	for template_dir in (settings.TEMPLATE_DIRS + app_template_dirs):
+		for dir, dirnames, filenames in os.walk(template_dir):
+			for filename in filenames:
+				template_files.append(os.path.join(dir, filename))
+	return 'aaa: '+template_files
+
 	#return render(request, 'index.html', configuration)
