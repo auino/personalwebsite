a6eafd328e469d2e8979b002386d8b5948d3e3d3
Enrico Cambiaso <enrico.cambiaso@gmail.com>
2019-03-29 10:34:41 +0100

    Minor bug fixes

--- personalwebsite/urls.py
+++ personalwebsite/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.contrib import admin
-
 from django.conf.urls import url, static
+from django.views.static import serve
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -10,22 +10,9 @@ from django.conf.urls import url, static
 from app.views import index
 
 urlpatterns = [
-    # Examples:
-    #url(r'^$', 'personalwebsite.views.index', name='home'),
-    #url(r'^$', 'app.views.index', name='home'),
     url(r'^$', index, name='home'),
-    # url(r'^personalwebsite/', include('personalwebsite.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    #url(r'^admin/', include(admin.site.urls)),
-
-    #(r'^static/', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    #static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT})
 ]
 
 urlpatterns += [
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT})
 ]
