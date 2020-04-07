e8d8f15a347f94c94089675823331f085274cf66
auino <enrico.cambiaso@gmail.com>
2020-04-07 15:14:27 +0200

    Added cache control

--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
 import urllib, json
 from django.shortcuts import render
+from django.views.decorators.cache import cache_control
 
 # main index view
+@cache_control(max_age=3600)
 def index(request):
 	BASEURL=request.build_absolute_uri()
 	url = BASEURL+'/static/configuration/configuration.json'
