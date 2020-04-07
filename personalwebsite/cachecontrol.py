8edfa19a1445ccb07eefef6424c3151fcb8d0ba8
auino <enrico.cambiaso@gmail.com>
2020-04-07 15:24:52 +0200

    Added cache control middleware

--- personalwebsite/cachecontrol.py
+++ personalwebsite/cachecontrol.py
@@ -1,5 +1,5 @@
 class CustomCache(object):
 	def process_response(self, request, response):
 		if request.path.startswith('/static/'):
-			response['Cache-Control'] = 'must-revalidate, no-cache'
+			response['Cache-Control'] = 'max-age=3600'
 		return response
